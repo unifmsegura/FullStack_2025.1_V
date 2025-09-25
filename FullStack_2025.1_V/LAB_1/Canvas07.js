897250638eb6cf94cdcaacf229fb3f233dc5de4c
unifmsegura <matheusbesegura@gmail.com>
2025-09-25 16:48:40 -0300

    UPDATE DO CANVAS LAB 05

--- FullStack_2025.1_V/LAB_1/Canvas07.js
+++ FullStack_2025.1_V/LAB_1/Canvas07.js
@@ -46,7 +46,7 @@ quadrado(270, 270, 30, "black");
 linha(0, 0, 150, 150, "blue");
 linha(300, 0, 150, 150, "red");
 
-linha(150, 0, 150, 300, "black");
+linha(150, 150, 150, 260, "black");
 linha(0, 150, 300, 150, "green");
 
 texto("Canvas", 115, 50);
@@ -64,7 +64,7 @@ circulo(230, 220, 12, "yellow", "green");
 arco(150, 150, 30, Math.PI, 0, "green")
 arco(150, 150, 50, Math.PI, 0, "green");
 arco(150, 300, 40, Math.PI, 0, "green");
-arco(150, 300, 60, Math.PI, 0, "green");
+arco(108,310,50,1.5*Math.PI,2.5*Math.PI, 0, "green");
 
 // canvas 2
 const canvas2 = document.getElementById("canvas2");
@@ -76,13 +76,13 @@ ctx2.fillRect(0, 0, 300, 300);
 
 // SOL
 ctx2.beginPath();
-ctx2.arc(240, 60, 30, 0, 2 * Math.PI);
+ctx2.arc(230, 75, 35, 0, 2 * Math.PI);
 ctx2.fillStyle = "yellow";
 ctx2.fill();
 
 // CHÃO 
 ctx2.fillStyle = "gray";
-ctx2.fillRect(0, 210, 300, 100);
+ctx2.fillRect(0, 217, 300, 100);
 
 // LAGO CIRCULO
 function quadrado(x, y, size, cor) {
@@ -91,17 +91,17 @@ ctx.fillRect(x, y, size, size)
 }
 ctx2.fillStyle = "royalblue";
 ctx2.beginPath();
-ctx2.moveTo(0, 200);
+ctx2.moveTo(0, 218);
 ctx2.lineTo(60, 200);
-ctx2.arc(20, 230, 30, 1.5 * Math.PI, 0.5 * Math.PI);
-ctx2.lineTo(0, 260);
+ctx2.arc(5, 230, 45, 1 * Math.PI, 0 * Math.PI);
+ctx2.lineTo(0, 300);
 ctx2.closePath();
 ctx2.fill();
 
 ctx2.beginPath();
 ctx2.lineWidth = 2;
 ctx2.fillStyle = 'royalblue';
-ctx2.arc(105,310,50,1.5*Math.PI,2.5*Math.PI);
+ctx2.arc(108,310,50,1.5*Math.PI,2.5*Math.PI);
 ctx2.fill();
 ctx2.closePath();
 
@@ -109,10 +109,10 @@ ctx2.closePath();
 function desenharArvore(ctx, x, y) {
 // Tronco
 ctx.fillStyle = "saddlebrown";
-ctx.fillRect(x + 10, y + 20, 10, 30);
+ctx.fillRect(x + 10, y + 2, 15, 55);
 // Copa
 ctx.beginPath();
-ctx.arc(x + 15, y + 15, 15, 0, 2 * Math.PI);
+ctx.arc(x + 17.5, y + 10, 23, 0, 2 * Math.PI);
 ctx.fillStyle = "green";
 ctx.fill();
 }
@@ -121,14 +121,14 @@ desenharArvore(ctx2, 250, 190);
 
 // CASA
 ctx2.fillStyle = "sienna";
-ctx2.fillRect(120, 150, 60, 60); 
+ctx2.fillRect(115.5, 150, 69, 67); 
 
 // PORTA
 // RETÂNGULO
 ctx2.beginPath();
 ctx2.lineWidth = 2;
 ctx2.fillStyle = 'maroon';
-ctx2.fillRect(145,175,10,35);
+ctx2.fillRect(143.7,181,13,36);
 ctx2.closePath();
 
 ctx2.beginPath();
@@ -145,14 +145,14 @@ ctx2.closePath();
 
 // TELHADO
 ctx2.beginPath();
-ctx2.moveTo(120, 150);
-ctx2.lineTo(180, 150);
-ctx2.lineTo(150, 120);
+ctx2.moveTo(115, 150);
+ctx2.lineTo(185, 150);
+ctx2.lineTo(150, 110);
 ctx2.closePath();
 ctx2.fillStyle = "tomato";
 ctx2.fill();
 
 // JANELAS
 ctx2.fillStyle = "skyblue";
-ctx2.fillRect(130, 160, 15, 15);
-ctx2.fillRect(155, 160, 15, 15);
+ctx2.fillRect(122, 161, 21, 20);
+ctx2.fillRect(157, 161, 21, 20);
